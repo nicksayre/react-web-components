5b00d182cef075e8713251019553a1463405e39c
Nick Sayre <nick.sayre@adhocteam.us>
2022-03-29 15:28:01 -0500

    add dropdown web component and wrapper React component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 
+import { useState } from 'react';
 import './App.css';
-import Greeting from './Greeting.js';
+import { Dropdown } from './components/Dropdown.js';
 
 function App() {
-  console.log(Greeting)
+
+  let [title, setTitle] = useState('Click me')
+  let [show, setShow] = useState(false)
+
+  // function to run when
+  function handleShow(e) {
+    setShow(e.detail)
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <h1>React x Web Components</h1>
-        <greeting-component></greeting-component>
+        <h1>React and Web Components</h1>
+        <h2>(Best friends forever)</h2>
+        <p>The dropdown is {show ? 'open' : 'closed'}</p>
+        <Dropdown
+          title={title}
+          onShow={ (e) => handleShow(e) }
+        >
+          🤠 Howdy. This is passed down to the web component from React
+        </Dropdown>
       </header>
     </div>
   );
---
+++ src/components/Dropdown.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+//import Greeting from './Greeting.js';
+import XDropdown from './x-dropdown.js';
+
+export class Dropdown extends Component {
+  constructor(props) {
+    super(props);
+    this.dropdownRef = React.createRef();
+  }
+
+  componentDidMount() {
+    this.dropdownRef.current.title = this.props.title;
+
+    if (this.props.onShow) {
+      this.dropdownRef.current.addEventListener('show', e =>
+        this.props.onShow(e)
+      );
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    if (this.props.title !== prevProps.title) {
+      this.dropdownRef.current.title = this.props.title;
+    }
+
+    if (this.props.show !== prevProps.show) {
+      this.dropdownRef.current.show = this.props.show;
+    }
+  }
+
+  render() {
+    //return <greeting-component ref={this.dropdownRef}>{this.props.children}</greeting-component>;
+    return (
+      <x-dropdown ref={this.dropdownRef}>
+        {this.props.children}
+      </x-dropdown>
+    )
+  }
+}
